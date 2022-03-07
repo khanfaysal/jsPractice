257e717c7f893e290dc9811d46d28fd70282b0e7
lincoln khan <mdfaysalkhancse@gmail.com>
2022-03-07 23:49:29 +0600

    create js practice repo for updated js array, functions and others

---
+++ arrayMission.js
@@ -0,0 +1,15 @@
+// 1. mixed type array declaration
+
+const mixedTypes = [1, 'faysal', [1, 2, 3], { name: 'faysal' }];
+console.log(mixedTypes);
+
+// 2. length check array
+
+const arrayLengthCheck = [1, 'faysal', [1, 2, 3], { name: 'faysal' }];
+console.log(arrayLengthCheck[1]);
+
+// 3. push method in array
+
+const colleagueNames = ['Mahadi', 'Ataur', 'Opu', 'Al-amin', 'Shuvo'];
+colleagueNames.pop();
+console.log(colleagueNames);
---
+++ functionMission.js
@@ -0,0 +1,46 @@
+// Basic function to console log message
+
+function greetings() {
+    console.log("Hello world")
+}
+greetings();
+
+// p
+const user = ['faysal', 'khan', 'frontend Engineer'];
+
+function getUserDetails([firstName, LastName, Profession]) {
+    return `${firstName} ${LastName} is a ${Profession}`
+}
+getUserDetails(user);
+
+
+// function AddNumber(number) {
+//     return number + 20;
+// }
+// const number = AddNumber(1);
+// console.log(number)
+
+
+// function birthYear(age) {
+//     return 2022 - age;
+// }
+// const calAge = birthYear(1995);
+// console.log(calAge);
+
+// const yearsToRetirement = (birthyear, firstName) => {
+//     const age = 2021 - birthyear;
+//     const retirement = 60 - age;
+//     return `${firstName} will be retired in ${retirement} years!`;
+// }
+
+// const retirement = yearsToRetirement(1986, 'Sumudu');
+// console.log(retirement);
+
+// const yaersToRetirement = (birthYear, personName) => {
+//     const age = 2021 - birthYear;
+//     const retirement = 60 - age;
+//     return `${personName} will be retired in ${retirement} years`
+// }
+// const retirement = yaersToRetirement(1995, "faysal");
+// console.log(retirement);
+
