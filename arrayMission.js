1adb992998a81b8a4670249e87f078e6f57556d6
lincoln khan <mdfaysalkhancse@gmail.com>
2022-03-08 01:28:16 +0600

    added push, pop, unshift, shift, reverse, sort, splice and check copywithin and fill methods

--- arrayMission.js
+++ arrayMission.js
@@ -3,13 +3,57 @@
 const mixedTypes = [1, 'faysal', [1, 2, 3], { name: 'faysal' }];
 console.log(mixedTypes);
 
-// 2. length check array
+// 2. length and index element check array
 
 const arrayLengthCheck = [1, 'faysal', [1, 2, 3], { name: 'faysal' }];
-console.log(arrayLengthCheck[1]);
+console.log(arrayLengthCheck.length);
+console.log(arrayLengthCheck[0]);
 
 // 3. push method in array
 
 const colleagueNames = ['Mahadi', 'Ataur', 'Opu', 'Al-amin', 'Shuvo'];
-colleagueNames.pop();
+colleagueNames.push('kripa');
 console.log(colleagueNames);
+
+// 3. pop method in array
+
+const BrotherNames = ['lekhon', 'Mahadi', 'Ataur', 'Opu', 'Al-amin', 'Shuvo'];
+BrotherNames.pop();
+console.log(BrotherNames);
+
+// 4. unshift method in array
+
+const fruits = ['apple', 'jack-fruit', 'banana', 'lemon'];
+fruits.unshift('pineapple');
+console.log(fruits)
+// output : [ 'pineapple', 'apple', 'jack-fruit', 'banana', 'lemon' ]
+
+// 5. shift method in array
+
+const fruitList = ['apple', 'jack-fruit', 'banana', 'lemon'];
+fruitList.shift();
+console.log(fruitList)
+// output : [ 'jack-fruit', 'banana', 'lemon' ]
+
+// 6. reverse method in array
+
+const numberReverse = [1, 2, 3, 4, 5, 6];
+numberReverse.reverse();
+console.log(numberReverse)
+// output : [ 6, 5, 4, 3, 2, 1 ]
+
+// 7. sort method in array (according to ascending or descending)
+
+const numberSort = [10, 2, 3, 43, 15, 6];
+numberSort.sort();
+console.log(numberSort)
+// output : [ 6, 5, 4, 3, 2, 1 ]
+
+// 7. splice method in array
+
+const numberSplice = [10, 2, 3, 43, 15, 6];
+numberSplice.splice(3, 1);
+// numberSplice.splice(3, 1, 15);
+console.log(numberSplice)
+// output : [ 6, 5, 4, 3, 2, 1 ]
+
